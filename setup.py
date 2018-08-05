4802c559cd7b0f184b0d5ec245297f607e5125a6
Jakub Valenta <jakub@jakubvalenta.cz>
2018-08-05 22:50:57 +0200

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='video_composer',
 
-    version='1.0.1',
+    version='1.1.0',
 
     description='Video composer.',
     long_description=long_description,
